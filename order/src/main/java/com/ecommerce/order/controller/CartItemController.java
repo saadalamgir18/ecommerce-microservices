11fece75168a3a47239667ab7965eb9b38f0b9c2
saadalamgir18 <saadalamgir18@gamil.com>
2025-07-01 17:16:06 +0500

    added rate limiting, retry machanism

--- order/src/main/java/com/ecommerce/order/controller/CartItemController.java
+++ order/src/main/java/com/ecommerce/order/controller/CartItemController.java
@@ -4,11 +4,13 @@ package com.ecommerce.order.controller;
 import com.ecommerce.order.dto.CartItemRequest;
 import com.ecommerce.order.dto.CartItemResponse;
 import com.ecommerce.order.service.CartItemService;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -38,9 +40,21 @@ public class CartItemController {
     }
 
     @GetMapping
+    @RateLimiter(name = "rateBreaker", fallbackMethod = "getCartItemsFallback")
     public ResponseEntity<List<CartItemResponse>> getCartItems(@RequestHeader("X-User-ID") String userId) {
+        System.out.println(userId);
         List<CartItemResponse> cartItems = cartItemService.findAll(userId);
         return ResponseEntity.ok(cartItems);
     }
+
+    public ResponseEntity<List<CartItemResponse>> getCartItemsFallback(String userId, Throwable t) {
+        // You can log the exception `t` if needed
+        List<CartItemResponse> fallbackResponse = new ArrayList<>();
+        return ResponseEntity
+                .status(HttpStatus.SERVICE_UNAVAILABLE)
+                .body(fallbackResponse);
+    }
+
+
 
 }
